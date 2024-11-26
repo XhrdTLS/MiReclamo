954caa2ae5b2a83a6079a85b2b5709aec80b0b12
Tomás L <59000698+XhrdTLS@users.noreply.github.com>
2024-11-26 09:46:29 -0300

    feature: Refresh state (#8)
    - Se cambian las imágenes de la app
    - Se agrega skeletonizer

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -3,9 +3,6 @@ import 'package:mi_reclamo/core/widgets/styles/theme.dart';
 import 'package:mi_reclamo/features/presentation/pages/login/login_page.dart';
 import 'core/core.dart';
 
-
-
-
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   // await initializeTickets();
